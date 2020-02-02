3ced345801b8e2cb778672223e4aaf7dee49fab2
Jesse Yuille <Jesseyuille@yahoo.com>
2020-02-01 20:07:06 -0500

    Sort/display high scores, game select tabs

--- angular-project-two/src/app/leaderboard/leaderboard.component.ts
+++ angular-project-two/src/app/leaderboard/leaderboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Scores, MOCK_SCORES } from '../mockScores'
 
 @Component({
   selector: 'app-leaderboard',
@@ -6,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class LeaderboardComponent implements OnInit {
+
+scores = MOCK_SCORES.sort(function(a,b) {
+ return b.score - a.score;
+});
+
+//Change mock scores to global High scores when available
+highscores = MOCK_SCORES.sort(function(a,b) {
+  return b.score - a.score;
+ });
 
   constructor() { }
 
---
+++ angular-project-two/src/app/mockScores.ts
@@ -0,0 +1,12 @@
+export class Scores {
+    score: number;
+    name: string;
+}
+
+export const MOCK_SCORES = [
+    {score: 1123,name: "bob"},
+    {score: 2445, name: "joe"},
+    {score: 1835, name: "phil"},
+    {score: 1799, name: "jason"},
+    {score: 3221, name: "Xxnoob_slayerxX"}
+];
