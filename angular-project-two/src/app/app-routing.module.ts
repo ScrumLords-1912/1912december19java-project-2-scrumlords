017a376ec949122ba3f86fcab9ee2df38161d9f3
Garrett Jones <mgjones130@gmail.com>
2020-02-05 10:18:03 -0500

    styling and routing progress. still needs work

--- angular-project-two/src/app/app-routing.module.ts
+++ angular-project-two/src/app/app-routing.module.ts
@@ -3,17 +3,35 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { AuthGuard } from './auth.guard';
 import { LeaderboardComponent } from './leaderboard/leaderboard.component';
+import { ProfileComponent } from './profile/profile.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { Cardpage3Component } from './cardpage3/cardpage3.component';
 
 
 
 const routes: Routes = [
   {
-    path: '',
-    component: LoginComponent
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'match',
+    component: Cardpage3Component,
+    canActivate: [AuthGuard]
   },
   {
     path: 'leaderboard',
     component: LeaderboardComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'info',
+    component: CarouselComponent,
     canActivate: [AuthGuard]
   },
 ];
