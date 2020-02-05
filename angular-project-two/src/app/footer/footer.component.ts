58f57a55d93481fa347ddbce6caa48207f1a3f4b
Garrett Jones <mgjones130@gmail.com>
2020-02-04 21:17:24 -0500

    navbar tweaks, footer done?

--- angular-project-two/src/app/footer/footer.component.ts
+++ angular-project-two/src/app/footer/footer.component.ts
@@ -3,7 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styles: []
+  styles: [
+    'footer {color:aliceblue;position:fixed;bottom:0px;margin:0px;line-height:30px;}',
+  ],
 })
 export class FooterComponent implements OnInit {
 
