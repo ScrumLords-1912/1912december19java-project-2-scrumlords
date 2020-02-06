c9ca19c6561fdd596db7d359f59b64facb9e4c76
Garrett Jones <mgjones130@gmail.com>
2020-02-06 14:03:20 -0500

    linked to real endpoints

--- angular-project-two/src/app/user.service.ts
+++ angular-project-two/src/app/user.service.ts
@@ -14,7 +14,7 @@ export class UserService {
   public loggedInUser: User = new User(0, '', '');
 
   register(id:number, username: string, password: string){
-    this.http.post('http://localhost:8080/login/create-account', new User(id, username, password))
+    this.http.post('http://ec2-3-90-146-246.compute-1amazonaws.com:8081/login/create-account', new User(id, username, password))
     .subscribe((response)=>{
       console.log(`registerd as user ${response}`);
     });
@@ -22,7 +22,7 @@ export class UserService {
 
   attemptLogIn(id:number, username: string, password: string) {
     const loggingInAsUser = new User(id ,username, password)
-    this.http.post('http://localhost:8080/login', loggingInAsUser /**new User(username, password)**/)
+    this.http.post('http://ec2-3-90-146-246.compute-1amazonaws.com:8081/login', loggingInAsUser /**new User(username, password)**/)
       .subscribe((response: boolean) => {
         if (response) {
           this.isLoggedIn = true;
@@ -35,7 +35,7 @@ export class UserService {
   }
 
   updateProfile(user: User) {
-    this.http.post('http://localhost:8080/profile', user)
+    this.http.post('http://ec2-3-90-146-246.compute-1amazonaws.com:8081/profile', user)
       .subscribe((response) => {
         console.log(`this is the response = ${response}`);
       });
